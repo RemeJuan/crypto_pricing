2d15ce7250f2d8608d3b80689e4fc1ef83a87d54
remelehane <reme.lehane@gmail.com>
2021-04-27 10:08:09 +0200

    feat(core): added base usecase

---
+++ lib/core/domain/usecases/usecase.dart
@@ -0,0 +1,8 @@
+import 'package:crypto_pricing/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
+
+class NoParams {}

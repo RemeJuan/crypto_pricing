3ac6a698efa1de71ddef7e4cef633501ffc3d008
remelehane <reme.lehane@gmail.com>
2021-04-27 10:11:20 +0200

    none(coverage): updated filter context

--- coverage_test_report.sh
+++ coverage_test_report.sh
@@ -1,7 +1,7 @@
 run_coverage() {
   # shellcheck disable=SC2034
   STATE=$1
-  flutter pub run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '(.gr?|.freezed|_services?|_helpers?|_controller_view|_search_delegate).dart$'
+  flutter pub run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '(.gr?|.freezed|_helpers?).dart$'
 
   genhtml -o coverage coverage/lcov.info -q
 }

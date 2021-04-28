7b4df067ee3e279a3cd67d847f8df1320141cbf9
remelehane <reme.lehane@gmail.com>
2021-04-28 20:44:55 +0200

    feat(app): updated remote source path and query params

--- lib/data/sources/markets/markets_remote_source.dart
+++ lib/data/sources/markets/markets_remote_source.dart
@@ -15,18 +15,26 @@ class MarketsRemoteSourceImpl implements MarketsRemoteSource {
     required this.networkManager,
   });
 
-  static final String _path = 'tickers';
+  static final String _path = 'coins/markets';
 
   @override
   Future<MarketsEntity> getMarkets() async {
+    const qp = <String, String>{
+      'vs_currency': 'usd',
+      'order': 'market_cap_desc',
+      'per_page': '20',
+      'page': '1',
+      'sparkline': 'false',
+      'price_change_percentage': '1h,24h,7d',
+    };
+
     final url = EndpointBuilder.url(
       _path,
-      queryParameters: <String, dynamic>{'limit': '20'},
+      queryParameters: qp,
     );
 
     final response = await networkManager.apiGet(url);
     final decoded = jsonDecode(response.body) as List;
-
     return MarketsEntity.fromJson(<String, dynamic>{'data': decoded});
   }
 }
--- test/data/sources/markets_remote_source_test.dart
+++ test/data/sources/markets_remote_source_test.dart
@@ -21,7 +21,8 @@ void main() {
   });
 
   group('getMarkets', () {
-    const mockUrl = 'https://api.coinlore.net/api/tickers?limit=20';
+    const mockUrl =
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d';
     setUp(() {
       when(mockNetworkManager.apiGet(any)).thenAnswer(
         (_) async => http.Response(data, 200),
